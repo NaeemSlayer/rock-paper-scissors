226ca3c3d5022bd1bc29a1e5a702cead14568a4a
NaeemSlayer <140403182+NaeemSlayer@users.noreply.github.com>
2024-02-19 22:28:15 +0500

    Added function to play a single round of rps

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+// function to get computers choice randomly
 function getComputerChoice() {
-    const choises = [ "Rock","Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random() * choises.length);
-    return choises[randomIndex];
+    const choices = [ "Rock","Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+// function to play a single round of rps
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (
+        (playerSelection === 'rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'Paper')
+        ) {
+            return 'You Win!' + playerSelection + 'beats' + computerSelection;
+        } else if (playerSelection === computerSelection) {
+            return 'It\'s a tie';
+        } else {
+            return 'You Lose!' + computerSelection + 'beats' + playerSelection;
+        }
 }
